083d5a806ccead2384d00387a9d6d3e13e49100d
Sivaraman Ganesh <sivacanvas.sg@gmail.com>
2024-04-27 21:31:28 +0530

    Add OVP app

---
+++ purchasedocumentovp/webapp/Component.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("mm.purchasedocumentovp.Component", {
+            // New changes
+            // Analytical apps
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
